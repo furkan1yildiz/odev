702e92b5bcc5fdb8fee27c8b86376b2de9d3ab92
Muhammed Bayraktar <mhmmd.bayraktar@gmail.com>
2019-01-18 18:30:06 +0300

    silindi

--- main.js
+++ main.js
@@ -4,10 +4,4 @@ function login()
 
     }
 
-    function openForm() {
-        document.getElementById("myForm").style.display = "block";
-      }
-      
-      function closeForm() {
-        document.getElementById("myForm").style.display = "none";
-      }
+    

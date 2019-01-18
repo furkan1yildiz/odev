e44f6172fc8ff6bb6edc90aa8c3c8ce32b5a9ade
furkan1yildiz <45845067+furkan1yildiz@users.noreply.github.com>
2019-01-18 18:16:49 +0300

    arama butonu jquery

---
+++ main.js
@@ -0,0 +1,19 @@
+sayac=0;
+function basla(){
+    $('#kayip').hide();
+
+}
+function ara(){
+  
+    if(sayac==0){
+   $('#kayip').show();
+   sayac++;
+}
+  else if(sayac==1){
+      $('#kayip').hide();
+      sayac--;
+  }
+}
+
+
+
